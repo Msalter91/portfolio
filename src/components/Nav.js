2f7dd808a86e4139171deace7b65b346574908f5
Mike Salter <mikey.l.salter@gmail.com>
2022-02-19 16:02:43 +0000

    mobile nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,40 @@
+import react from 'react'
+import { FaHamburger } from 'react-icons/fa'
 
 function Nav () {
+
+  const [isShowingHamburger, setIsShowingHamburger] = react.useState(false)
+
+  const handleClick = () => {
+    setIsShowingHamburger(!isShowingHamburger)
+  }
+
   return (
-    <nav className="nav">
-      <div>
-        <a href='#'>Mike Salter </a>
-      </div>
-      <div>
-        <a href="#bio">Bio </a>
-        <a href="#skills">Skills </a>
-        <a href="#projects">Projects </a>
-        <a href="#interests">Non-tech stuff </a>
-      </div>
-    </nav>
+    <>
+      <nav className="nav">
+        <div>
+          <a href='#'>Mike Salter </a>
+        </div>
+        <div className='full-nav'>
+          <a href="#bio">Bio </a>
+          <a href="#skills">Skills </a>
+          <a href="#projects">Projects </a>
+          <a href="#interests">Non-tech stuff </a>
+        </div>
+        <FaHamburger className='mobile-nav' onClick={handleClick} />
+      </nav>
+      { isShowingHamburger &&      
+       <div className='mobile-nav-list'>
+         <a onClick={handleClick} href="#bio">Bio </a>
+         <a onClick={handleClick} href="#skills">Skills </a>
+         <a onClick={handleClick} href="#projects">Projects </a>
+         <a onClick={handleClick} href="#interests">Non-tech stuff </a>
+       </div>
+
+      }
+
+    </>
+
   )
 }
 
